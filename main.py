cfb533f84eb178cbd3b6abe25d8452fe92bfe1a8
Matt Compton <matt@getcryst.al>
2023-01-31 19:03:16 -0500

    purge repl stuff

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+from time import sleep
+import random
+
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
@@ -11,6 +14,7 @@ driver = webdriver.Chrome(options=chrome_options)
 
 def try_find_addr(query):
   driver.get("https://google.com")
+  sleep(random.uniform(0.0,1.2))
 
   box = driver.find_element(By.CLASS_NAME, 'gLFyf')
   box.send_keys(query + "\n")
@@ -26,11 +30,10 @@ df = pd.read_csv("data.csv")
 
 enmpty = df[ (df['Address'].isnull()) ].index
 
-c = 0
+changed = 0
+failed = 0
 
 for index in enmpty:
-  if c == 3:
-    break
   print(f"Found empty: {str(df.loc[index, 'School:'])} at index {str(index)}")
   school = str(df.loc[index, 'School:'])
   state = str(df.loc[index, 'State:'])
@@ -38,10 +41,15 @@ for index in enmpty:
   if addr != "FAIL":
     df.loc[index, 'Address'] = addr
     print(f"Updated address for {school} in {state} to: '{addr}'")
+    df.to_csv("data_save.csv", index=False)
+    changed += 1
   else:
     print(f"Couldn't get an address for {school} in {state}")
-  c += 1
+    failed += 1
+  print(f"Status: Updated: {str(changed)}, Failed: {str(failed)}")
+
 
 df.to_csv("data_save.csv", index=False)
 
 print("Done.")
+print("Data in new file")
